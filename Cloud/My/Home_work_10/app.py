e8ef23ce0f8517963f7918d3ea68cc401e24a353
Вадим <niras.sp@gmail.com>
2022-05-18 09:43:43 +0700

    Завершение домашней работы 10

---
+++ Cloud/My/Home_work_10/app.py
@@ -0,0 +1,26 @@
+def main():
+    import utils
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def page_index():
+        return f'<pre>{utils.data_string()}</pre>'
+
+    @app.route('/candidates/<int:x>')
+    def page_candidates(x):
+        url = utils.return_dict(x)['picture']
+        return f"<img src='{url}'>\n\n" \
+               f"<pre>Имя кандидата - {utils.return_dict(x)['name']}\n" \
+               f"Позиция кандидата - {utils.return_dict(x)['position']}\n" \
+               f"Навыки - {utils.return_dict(x)['skills']}</pre>"
+
+    @app.route('/skills/<x>')
+    def page_skills(x):
+        return f'<pre>{utils.search_skills(x)}</pre>'
+
+    app.run(host='127.0.0.6', port=80)
+
+
+main()
---
+++ Cloud/My/Home_work_10/utils.py
@@ -0,0 +1,34 @@
+import json
+
+
+def data_string():
+    """Вернёт данные по кандидатам в строке"""
+    dict_string = ''
+    for i in dictionary:
+        dict_string += f"Имя кандидата - {i['name']}\n" \
+                       f"Позиция кандидата - {i['position']}\n" \
+                       f"Навыки - {i['skills']}\n\n"
+    return dict_string[:-2]
+
+
+def return_dict(id_):
+    """Получение словаря из списка словарей по id"""
+    for i in dictionary:
+        if i['id'] == id_:
+            return i
+
+
+def search_skills(skill):
+    """Получение кандидатов с навыком"""
+    cand_skill = ''
+    for i in dictionary:
+        if skill.lower() in i['skills'].lower():
+            cand_skill += f"Имя кандидата - {i['name']}\n" \
+                          f"Позиция кандидата - {i['position']}\n" \
+                          f"Навыки - {i['skills']}\n\n"
+    return cand_skill[:-2]
+
+
+file = open('candidates.json')
+dictionary = json.loads(file.read())
+file.close()
