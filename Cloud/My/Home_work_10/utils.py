55f40f4fa448831fa6664bb9c2eb318f68147f5f
Вадим <niras.sp@gmail.com>
2022-05-21 07:51:55 +0700

    Добавление документации для точного поиска по навыку

--- Cloud/My/Home_work_10/utils.py
+++ Cloud/My/Home_work_10/utils.py
@@ -29,6 +29,16 @@ def search_skills(skill):
             return cand_skill[:-2]
     else:
         return 'Нет кандидата с таким навыком'
+#     для точного поиска по навыку с 26 строки:
+#             for j in i['skills'].lower().split(', '):
+#                 if j == skill.lower():
+#                     cand_skill += f"Имя кандидата - {i['name']}\n" \
+#                                   f"Позиция кандидата - {i['position']}\n" \
+#                                   f"Навыки - {i['skills']}\n\n"
+#     if cand_skill != '':
+#         return cand_skill[:-2]
+#     else:
+#         return 'Нет кандидата с таким навыком'
 
 
 #  загрузим словарь с данными
