163133fe28061ff6de3ce483e91e816177b85350
Вадим <niras.sp@gmail.com>
2022-05-25 08:08:26 +0700

    Домашка 11 1.0

--- Cloud/My/Home_work_11/part_2/app.py
+++ Cloud/My/Home_work_11/part_2/app.py
@@ -1,24 +1,44 @@
 import utils
 from flask import Flask, render_template
 
+# Объявим Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  # обозначим главную страницу со ссылками на кандидатов
 def page_index():
     dictionary = utils.dictionary
     return render_template("list.html", dictionary=dictionary)
 
 
-@app.route('/candidates/<int:x>')
+@app.route('/candidate/<int:x>')  # сами странички кандидатов
 def candidate(x):
-    # if type(utils.return_dict(x)) != dict:  # если функция вернёт не словарь, значит такого кандидата нет
-    #     return 'Нет такого кандидата в базе'
-    name = utils.return_dict(x)['name']  # имя кандидата
+    if type(utils.return_dict(x)) != dict:  # если функция вернёт не словарь, значит такого кандидата нет
+        return '<h3>Нет такого кандидата в базе</h3>'
+    name_ = utils.return_dict(x)['name']  # имя кандидата
     position = utils.return_dict(x)['position']  # позиция кандидата
     url = utils.return_dict(x)['picture']  # картинка в переменной
     skills = utils.return_dict(x)['skills']  # навыки кандидата
-    return render_template('card.html', name=name, position=position, url=url, skills=skills)
+    return render_template('card.html', name=name_, position=position, url=url, skills=skills)
+
+
+@app.route('/search/<candidate_name>')  # поиск по именам кандидатов
+def name_cand(candidate_name):
+    if type(utils.search_name(candidate_name)) != list:  # если функция вернёт не список, значит такого кандидата нет
+        return '<h1>Найдено кандидатов: 0</h1>'
+    len_results = len(utils.search_name(candidate_name))  # количество найденных кандидатов
+    results_dict = utils.search_name(candidate_name)  # список словарей с найденными кандидатами
+    return render_template('search.html', results_dict=results_dict, len_results=len_results)
+
+
+@app.route('/skill/<skill_name>')  # поиск по навыкам кандидатов
+def skill_cand(skill_name):
+    if type(utils.search_skills(skill_name)) != list:  # если функция вернёт не список, значит такого кандидата нет
+        return '<h1>Найдено кандидатов: 0</h1>'
+    len_results = len(utils.search_skills(skill_name))  # количество найденных кандидатов
+    results_dict = utils.search_skills(skill_name)  # список словарей с найденными кандидатами
+    return render_template('skill.html', results_dict=results_dict, len_results=len_results)
 
 
 app.run(host='127.0.0.6', port=80)
--- Cloud/My/Home_work_11/part_2/utils.py
+++ Cloud/My/Home_work_11/part_2/utils.py
@@ -1,20 +1,5 @@
 import json
 
-#  загрузим словарь с данными
-file = open('candidates.json')
-dictionary = json.loads(file.read())
-file.close()
-
-
-def data_string():
-    """Вернёт данные по кандидатам в строке"""
-    dict_string = ''
-    for i in dictionary:
-        dict_string += f"Имя кандидата - {i['name']}\n" \
-                       f"Позиция кандидата - {i['position']}\n" \
-                       f"Навыки - {i['skills']}\n\n"
-    return dict_string[:-2]
-
 
 def return_dict(id_):
     """Получение словаря кандидата по id"""
@@ -24,28 +9,24 @@ def return_dict(id_):
 
 
 def search_skills(skill):
-    """Получение кандидатов с навыком"""
-    cand_skill = ''
+    """Получение списка словарей кандидатов с подходящим скиллом"""
+    cand_list_dict = []
     for i in dictionary:
         if skill.lower() in i['skills'].lower():
-            cand_skill += f"Имя кандидата - {i['name']}\n" \
-                          f"Позиция кандидата - {i['position']}\n" \
-                          f"Навыки - {i['skills']}\n\n"
-    if cand_skill != '':
-        return cand_skill[:-2]
-    else:
-        return 'Нет кандидата с таким навыком'
+            cand_list_dict.append(i)
+    return cand_list_dict
 
 
 def search_name(name):
-    """Получение кандидата по имени"""
-    cand_name = ''
+    """Получение списка словарей кандидатов с подходящим именем"""
+    cand_list_dict = []
     for i in dictionary:
         if name.lower() in i['name'].lower():
-            cand_name += f"Имя кандидата - {i['name']}\n" \
-                          f"Позиция кандидата - {i['position']}\n" \
-                          f"Навыки - {i['skills']}\n\n"
-    if cand_name != '':
-        return cand_name[:-2]
-    else:
-        return 'Нет кандидата с таким именем'
+            cand_list_dict.append(i)
+    return cand_list_dict
+
+
+#  загрузим словарь с данными
+file = open('candidates.json')
+dictionary = json.loads(file.read())
+file.close()
