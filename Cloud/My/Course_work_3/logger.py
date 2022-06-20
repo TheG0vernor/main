69a9b4bb6c997e99708899ea8f6368ac4c5997db
Vadim Lysenko <32599435+TheG0vernor@users.noreply.github.com>
2022-06-20 18:21:42 +0700

    Delete Cloud/My/Course_work_3 directory

--- Cloud/My/Course_work_3/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-api_log = 'api.log'
-logging.basicConfig(level=logging.INFO)
-logger_api = logging.getLogger('Вызываемый')  # объявим логгер
-FileHandler = logging.FileHandler(f'C:\Project_Python\Cloud\My\Course_work_3\logs\\api.log')  # объявим хэндлер
-formatter_one = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')  # объявим форматирование
-FileHandler.setFormatter(formatter_one)  # применим форматирование к хэндлеру
-logger_api.addHandler(FileHandler)  # применим хэндлер к логгеру
